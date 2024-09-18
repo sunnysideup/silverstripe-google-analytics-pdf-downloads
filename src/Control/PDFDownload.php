409763875d42bce25e757604b41d86094dcccac9
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-09-19 09:27:26 +1200

    MNT: FIRST COMMIT

--- src/Control/PDFDownload.php
+++ src/Control/PDFDownload.php
@@ -7,12 +7,19 @@ use SilverStripe\Control\HTTPRequest;
 
 class PDFController extends Controller
 {
-    private static $allowed_actions = [
-        'topdf',
+    private static array $allowed_actions = [
+        'topdf' => true,
     ];
+
+    private static string  $ga_code = '';
 
     public function topdf(HTTPRequest $request)
     {
         return $this->renderWith(self::class);
+    }
+
+    public function GACode()
+    {
+        return $this->Config()->get('ga_code');
     }
 }
